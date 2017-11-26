4c6c855f8aa0fdb26584ce327499a32c53c73198
Nicolas Märchy <nm@studer-raimann.ch>
2017-11-26 16:29:18 +0100

    Create report factory

--- src/main/java/ch/schulealtendorf/pra/ReportAPIFactory.java
+++ src/main/java/ch/schulealtendorf/pra/ReportAPIFactory.java
@@ -1,8 +1,30 @@
 package ch.schulealtendorf.pra;
 
+import ch.schulealtendorf.pra.api.*;
+
 /**
  * @author nmaerchy
  * @version 0.0.1
  */
 public class ReportAPIFactory {
+    
+    public static TotalRankingAPI getTotalRanking() {
+        throw new UnsupportedOperationException("This method is not implemented yet.");
+    }
+    
+    public static DisciplineRankingAPI getDisciplineRanking() {
+        throw new UnsupportedOperationException("This method is not implemented yet.");
+    }
+    
+    public static DisciplineGroupRankingAPI getDisciplineGroupRanking() {
+        throw new UnsupportedOperationException("This method is not implemented yet.");
+    }
+    
+    public static ParticipantListAPI getParticipantList() {
+        throw new UnsupportedOperationException("This method is not implemented yet.");
+    }
+    
+    public static EventSheetAPI getEventSheet() {
+        throw new UnsupportedOperationException("This method is not implemented yet.");
+    }
 }
